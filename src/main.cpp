4ceb92c7a2c75028fb7ae73209ed6571a428d7a8
anshgupta00 <anshgupta430@gmail.com>
2023-07-05 16:11:50 +0545

    initial commit try 2

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,5 @@
 
 int main() {
     std::cout << "Welcome to ElVis!! The Algorithm Visualizer." << std::endl;
-    std::cout << "TRY 1" << std::endl;
-    std::cout << "Pull Request !!" <<std::endl;
     return 0;
 }
