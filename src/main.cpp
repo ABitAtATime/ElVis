4f41b91b2041850e6dfdbb2d4a68f9cd9b97ddae
hsednass <moc.liamghsednas@gmail.com>
2023-07-07 13:49:13 +0545

    Random data aayo

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,13 @@
 #include <iostream>
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "ElVis");
+    sf::RenderWindow window(sf::VideoMode(1200, 900), "ElVis");
     // std::cout << "test";
     auto visualizer = SortVis(window);
     auto bubble = Bubble(visualizer);
-    bubble.run(); 
+    bubble.run();
+    while (window.isOpen()) {
+        visualizer.update();
+    } 
     return 0;
 }
