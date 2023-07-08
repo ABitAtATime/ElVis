2bf34b4837ad22af1bf89664825badac75fe0872
hsednass <moc.liamghsednas@gmail.com>
2023-07-08 19:25:33 +0545

    oop langlang, main rewrite

---
+++ src/algorithm/sorting/Sorting.cpp
@@ -0,0 +1,17 @@
+#include <random>
+
+#include "Algorithm/Sorting/sorting.h"
+
+void Sorting::randomData() {
+    mdata.clear();
+
+        // Random number generator
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<int> distribution(0, 100);
+
+        // Generate random data
+        for (size_t i = 0; i < mdataSize; ++i) {
+            mdata.push_back(distribution(gen));
+        }
+}
