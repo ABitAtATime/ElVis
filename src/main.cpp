8b0b5c5a1fb2795b0acfb355c6b70e68f36e0918
Sandeep-Poudel <poudelsandeep0105@gmail.com>
2023-07-18 17:02:07 +0545

    hanuman

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include "ElVis.h"
 #include "Algorithm/Sorting/quicksort.h"
 #include "Algorithm/Sorting/mergeSort.h"
+#include "Algorithm/Sorting/selectionsort.h"
+#include "Algorithm/Sorting/insertionsort.h"
+#include "Algorithm/Sorting/heapsort.h"
 #include "Algorithm/Sorting/bubble.h"
 
 int main()
@@ -10,27 +13,50 @@ int main()
     std::string choice;
     std::cout << "Input your choice:" << std::endl;
     std::cout << "1 --- Bubble sort" << std::endl;
-    std::cout << "2 --- MergeSort " << std::endl;
-    std::cout << "3 --- QuickSort" << std::endl;
-    std::cout<<"\n";
-    std::cout<<"Enter your Choice:    ";
+    std::cout << "2 --- Merge Sort " << std::endl;
+    std::cout << "3 --- Quick Sort" << std::endl;
+    std::cout << "4 --- Selection Sort" << std::endl;
+    std::cout << "5 --- Insertion Sort" << std::endl;
+    std::cout << "6 --- Heap Sort" << std::endl;
+    std::cout << "hanuman" << std::endl;
+
+    std::cout << "\n";
+    std::cout << "Enter your Choice:    ";
     std::cin >> input;
-    sf::RenderWindow window(sf::VideoMode(1800, 1000), "ElVis");
+    sf::RenderWindow window(sf::VideoMode(1820, 990), "ElVis");
     if (input == 1)
     {
-        Bubble placeholder(900, window);
+        Bubble placeholder(1000, window);
         ElVis elvis(window, placeholder);
         elvis.run();
     }
     else if (input == 2)
     {
-        MergeSort placeholder(900, window);
+        MergeSort placeholder(1000, window);
         ElVis elvis(window, placeholder);
         elvis.run();
     }
     else if (input == 3)
     {
-        Quicksort placeholder(900, window);
+        Quicksort placeholder(1000, window);
+        ElVis elvis(window, placeholder);
+        elvis.run();
+    }
+    else if (input == 4)
+    {
+        SelectionSort placeholder(1000, window);
+        ElVis elvis(window, placeholder);
+        elvis.run();
+    }
+    else if (input == 5)
+    {
+        InsertionSort placeholder(1000, window);
+        ElVis elvis(window, placeholder);
+        elvis.run();
+    }
+    else if (input == 6)
+    {
+        HeapSort placeholder(1000, window);
         ElVis elvis(window, placeholder);
         elvis.run();
     }
